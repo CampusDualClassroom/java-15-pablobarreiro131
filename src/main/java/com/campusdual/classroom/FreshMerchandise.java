aded02cf3f1d6350bb0b4883360addb7b11e86ae
Pablo <pablobarreiro131@gmail.com>
2025-05-23 08:55:43 +0200

    Ejercicio 15, completado y testeado.

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -1,8 +1,37 @@
 package com.campusdual.classroom;
 
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
 
+    private String localizacion;
+    private Date expirationDate;
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+
+    @Override
+    public String getSpecificData() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Localización: ").append(getLocation()).append(", Caducidad: ").append(getFormattedDate(getExpirationDate()));
+        return builder.toString();
+    }
+
+    public void printSpecificData(){
+        System.out.println(getSpecificData());
+    }
+
+    public Object getExpirationDate() {
+        return this.expirationDate;
+    }
+
+    public String getFormattedDate(Object expirationDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        return sdf.format(expirationDate);
+    }
 }
